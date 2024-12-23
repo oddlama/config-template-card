f3988faf9d852b3c9c52479996c04ac072c5a1e4
Paul Donohue <git@PaulSD.com>
2024-12-22 23:10:11 -0500

    Simplify _evaluateConfig() and combine with _evaluateArray()

--- src/config-template-card.ts
+++ src/config-template-card.ts
@@ -175,45 +175,25 @@ export class ConfigTemplateCard extends LitElement {
 
   /* eslint-disable @typescript-eslint/no-explicit-any */
   private _evaluateConfig(config: any): any {
-    Object.entries(config).forEach((entry) => {
-      const key = entry[0];
-      const value = entry[1];
-
-      if (value !== null) {
-        if (value instanceof Array) {
-          config[key] = this._evaluateArray(value);
-        } else if (typeof value === 'object') {
-          config[key] = this._evaluateConfig(value);
-        } else if (typeof value === 'string' && value.includes('${')) {
-          config[key] = this._evaluateTemplate(value);
-        }
+    if (config instanceof Array) {
+      for (let i = 0; i < config.length; ++i) {
+        const value = config[i];
+        config[i] = this._evaluateConfig(value);
       }
-    });
+    } else if (typeof config === 'object') {
+      Object.entries(config).forEach(entry => {
+        const key = entry[0];
+        const value = entry[1];
+        config[key] = this._evaluateConfig(value);
+      });
+    } else if (typeof config === 'string' && config.includes('${')) {
+      return _evaluateTemplate(config);
+    }
 
     return config;
   }
 
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  private _evaluateArray(array: any): any {
-    for (let i = 0; i < array.length; ++i) {
-      const value = array[i];
-      if (value instanceof Array) {
-        array[i] = this._evaluateArray(value);
-      } else if (typeof value === 'object') {
-        array[i] = this._evaluateConfig(value);
-      } else if (typeof value === 'string' && value.includes('${')) {
-        array[i] = this._evaluateTemplate(value);
-      }
-    }
-
-    return array;
-  }
-
   private _evaluateTemplate(template: string): string {
-    if (!template.includes('${')) {
-      return template;
-    }
-
     /* eslint-disable @typescript-eslint/no-unused-vars */
     const user = this.hass ? this.hass.user : undefined;
     const states = this.hass ? this.hass.states : undefined;
